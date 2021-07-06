7666594937402c5d3584ea6595b734cf416bb4a7
jrantulm <jrantulm@NB-0323-075508>
2021-07-06 00:59:42 -0400

    Agregar archivos de proyecto.

---
+++ WebApiAutores2/Controllers/AutoresController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiAutores2.Entidades;
+
+namespace WebApiAutores2.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        /*
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() {
+                new Autor() { id = 1, Nombre = "Felipe"},
+                new Autor() { id = 2, Nombre = "Claudia"}
+            };
+        }*/
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post (Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")] //    api/autores/id
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if (autor.id != id)
+            {
+                return BadRequest("El id del autor no cincide con el id de la URL");
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+
+        }
+    }
+}
---
+++ WebApiAutores2/Migrations/20210627062252_inicial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiAutores2.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autores");
+        }
+    }
+}
